342966d7718dad81dd733d4ff160bcf6ffaff051
Xue Ning <s10267391@connect.np.edu.sg>
2025-01-26 21:54:19 +0800

    Added main program loop & display menu

--- S10270487_PRG2Assignment/Program.cs
+++ S10270487_PRG2Assignment/Program.cs
@@ -6,3 +6,57 @@
 
 using S10270487_PRG2Assignment;
 
+// Main Program
+while (true)
+{
+    DisplayMenu();
+    Console.WriteLine("Please select your option: ");
+    string option = Console.ReadLine();
+    if (option == "0") { break; }
+    else if (option == "1")
+    {
+
+    }
+    else if (option == "2")
+    {
+
+    }
+    else if (option == "3")
+    {
+
+    }
+    else if (option == "4")
+    {
+
+    }
+    else if (option == "5")
+    {
+
+    }
+    else if (option == "6")
+    {
+
+    }
+    else if (option == "7")
+    {
+
+    }
+}
+Console.WriteLine("Goodbye!")
+
+// Methods
+void DisplayMenu()
+{
+    Console.WriteLine("=============================================");
+    Console.WriteLine("Welcome to Changi Airport Terminal 5");
+    Console.WriteLine("=============================================");
+    Console.WriteLine("1. List All Flights");
+    Console.WriteLine("2. List Boarding Gates");
+    Console.WriteLine("3. Assign a Boarding Gate to a Flight");
+    Console.WriteLine("4. Create Flight");
+    Console.WriteLine("5. Display Airline Flights");
+    Console.WriteLine("6. Modify Flight Details");
+    Console.WriteLine("7. Display Flight Schedule");
+    Console.WriteLine("0. Exit");
+    Console.WriteLine("---------------------------");
+}
